501c53044ddb8bf1b5a0d38deba64afbc57b8255
França Mac Dowell <fmdss@ic.ufal.br>
2019-05-10 12:53:49 -0300

    fixing importings in maven

--- src/main/java/br/ufal/ic/academico/model/Student.java
+++ src/main/java/br/ufal/ic/academico/model/Student.java
@@ -1,10 +1,12 @@
 package br.ufal.ic.academico.model;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
+import lombok.Getter;
 
 @Getter
 public class Student {
 
     private String code;
     private String name;
+
+
 }
