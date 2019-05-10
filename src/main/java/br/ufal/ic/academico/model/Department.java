541109444eeeb1a7b43051b2dd4c20eb5af7862b
França Mac Dowell <fmdss@ic.ufal.br>
2019-05-10 12:51:09 -0300

    First commit

---
+++ src/main/java/br/ufal/ic/academico/model/Department.java
@@ -0,0 +1,4 @@
+package br.ufal.ic.academico.model;
+
+public class Department {
+}
---
+++ src/main/java/br/ufal/ic/academico/model/Student.java
@@ -0,0 +1,10 @@
+package br.ufal.ic.academico.model;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+@Getter
+public class Student {
+
+    private String code;
+    private String name;
+}
